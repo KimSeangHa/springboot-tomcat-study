4f4a4c0c31ed8781d8fbe0ad32a69163a4ee7177
kshma_mlhjhac <kshman94@wattsolution.co.kr>
2022-07-12 13:49:30 +0900

    springboot tomcat study

--- src/main/java/me/study/Application.java
+++ src/main/java/me/study/Application.java
@@ -52,4 +52,14 @@ public class Application {
 			e.printStackTrace();
 		}
 	}
+	
+	// springboot Application 추가 시, 내장 Servlet 자동 생성한다.
+	/* 
+	 * 2022.07.12 Spring Boot 인프런 강의
+	 * Spring AutoConfiguration Dependency 추가
+	 * Maven > spring-boot-autoconfigure > /META-INF/spring-autoconfigure-metadata.properties
+	 * 자동 설정으로 내장 설정 파일들을 만들어 tomcat이 구동된다.
+	 * 
+	 * spring 에서 구현하던 MVC를 하기 위해, Dispatcher Servlet 또한 auto Configure에서 처리한다. 
+	 */
 }
